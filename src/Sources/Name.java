524df5658c4d65c6cdcb159e8780e2b4a2a0ddfd
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-05-09 21:48:34 -0700

    done name class including test

---
+++ src/Sources/Name.java
@@ -0,0 +1,126 @@
+/**
+ * Name of Bank
+ *
+ * @author Andres Arevalo & Dustin & Shamin
+ * @version 1.0
+ */
+public class Name
+{
+    private static final int    INITIAL = 0;
+    private static final int    MAX_NAME_LENGTH =  45;
+    private static final String INVALID_NAME = "admin";
+    private static final char   CHARACTER_TO_CAPITALIZE_NEXT = ' ';
+    private static final int    LOOP_START_INDEX = 1;
+    private static final int    FIRST_INITIAL = 0;
+
+    private final String first;
+    private final String last;
+
+    /**
+     * Initialize the name class. Check if is valid first and last name.
+     * @param first first name.
+     * @param last last name.
+     */
+    public Name(final String first,
+                final String last)
+    {
+        if(isInValidFirstLastName(first))
+        {
+            throw new IllegalArgumentException("bad first name!");
+        }
+
+        if(isInValidFirstLastName(last))
+        {
+            throw new IllegalArgumentException("bad last name!");
+        }
+
+        this.first = first;
+        this.last = last;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(Name.getStringCapitalized("aNdReS aRevAlO"));
+    }
+
+    /**
+     * Checks if the given string is invalid. A string is invalid if:
+     * It's null, it's empty, it's equals or bigger than 45 characters or contain the word "admin"
+     * @param name string to be validated.
+     * @return true if is invalid name false if not.
+     */
+    private boolean isInValidFirstLastName(final String name)
+    {
+        return name == null || name.isBlank() || name.length() >= MAX_NAME_LENGTH || name.equalsIgnoreCase(INVALID_NAME);
+    }
+
+    /**
+     *
+     * @return initials concatenated (e.g. name of "tigER wooDS" would return "T.W.").
+     */
+    public String getInitials()
+    {
+        return Character.toUpperCase(first.charAt(INITIAL)) + "." + Character.toUpperCase(last.charAt(INITIAL)) + ".";
+    }
+
+    /**
+     *
+     * @return full name capitalized (e.g. name of "tigER wooDS" would return "Tiger Woods").
+     */
+    public String getFullName()
+    {
+        return getStringCapitalized(first + " " + last);
+    }
+
+    /**
+     *
+     * @return first name
+     */
+    public String getFirst()
+    {
+        return first;
+    }
+
+    /**
+     *
+     * @return last name
+     */
+    public String getLast()
+    {
+        return last;
+    }
+
+    /**
+     * Capitalize the given string.
+     * @param str the string to be capitalized.
+     * @return String capitalized.
+     */
+    public static String getStringCapitalized(final String str)
+    {
+        final char[] strArrayOfChars;
+        final String strCapitalized;
+        boolean      capitalize;
+
+        capitalize = false;
+
+        strArrayOfChars = str.trim().toLowerCase().toCharArray();
+        strArrayOfChars[FIRST_INITIAL] = Character.toUpperCase(strArrayOfChars[FIRST_INITIAL]);
+
+        for(int i = LOOP_START_INDEX; i < strArrayOfChars.length ; i++)
+        {
+            if(capitalize)
+            {
+                strArrayOfChars[i] = Character.toUpperCase(strArrayOfChars[i]);
+                capitalize = false;
+            }
+            if(strArrayOfChars[i] == CHARACTER_TO_CAPITALIZE_NEXT)
+            {
+                capitalize = true;
+            }
+        }
+
+        strCapitalized = new String(strArrayOfChars);
+
+        return strCapitalized;
+    }
+}
---
+++ src/Tests/NameTest.java
@@ -0,0 +1,165 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameTest
+{
+    Name n1;
+    Name n2;
+
+    @BeforeEach
+    void setUp()
+    {
+        n1 = new Name("tigER", "wooDS");
+        n2 = new Name("eLoN", "MuSk");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        n1 = null;
+        n2 = null;
+    }
+
+    @Test
+    void getInitials()
+    {
+        assertEquals("T.W.", n1.getInitials());
+        assertEquals("E.M.", n2.getInitials());
+    }
+
+    @Test
+    void getFullName()
+    {
+        assertEquals("Tiger Woods", n1.getFullName());
+        assertEquals("Elon Musk", n2.getFullName());
+    }
+
+    @Test
+    void getFirst()
+    {
+        Name name = new Name("tigER", "wooDS");
+        assertEquals("tigER", name.getFirst());
+
+        name = new Name("eLoN", "MuSk");
+        assertEquals("eLoN", name.getFirst());
+
+    }
+
+    @Test
+    public void constructorThrows_withBadArgument() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name(null, "wooDS");
+        });
+
+        String expectedMessage = "bad first name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void emptyFirstName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("", "wooDS");
+        });
+
+        String expectedMessage = "bad first name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void whiteSpaceFirstName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name(" ", "wooDS");
+        });
+
+        String expectedMessage = "bad first name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void containsAdminFirstName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("admin", "wooDS");
+        });
+
+        String expectedMessage = "bad first name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void tooLongFirstName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("asdasjdasdhdsajdjasdjasdjsdjasdajsdjasdjasdjasdjsadjasdjasdjasjdasjdasjdasjdasjdsajdasdjd", "wooDS");
+        });
+
+        String expectedMessage = "bad first name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void emptyLastName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("Tiger", "");
+        });
+
+        String expectedMessage = "bad last name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void whiteSpaceLastName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("TigeR", " ");
+        });
+
+        String expectedMessage = "bad last name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void containsAdminLastName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("Tiger", "admin");
+        });
+
+        String expectedMessage = "bad last name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void tooLongLastName() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Name name = new Name("Tiger", "asdasjdasdhdsajdjasdjasdjsdjasdajsdjasdjasdjasdjsadjasdjasdjasjdasjdasjdasjdasjdsajdasdjd");
+        });
+
+        String expectedMessage = "bad last name!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+
+
+
+
+
+
+}
